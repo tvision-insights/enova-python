262dc276ffa72c20487764cf7645eb0d6a15b9d4
Julio Lajara <julio@tvisioninsights.com>
2020-05-19 12:09:19 -0400

    Fix the import exceptions again, add a second exception.

--- files/halite_gem/poise_python/version.rb
+++ files/halite_gem/poise_python/version.rb
@@ -16,5 +16,5 @@
 
 
 module PoisePython
-  VERSION = '1.8.2'
+  VERSION = '1.8.3'
 end
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'api@enova.com'
 license          'Apache-2'
 description      'Installs/Configures python and modules'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.8.2'
+version          '1.8.3'
 
 depends          "poise","~> 2.7"
 depends          "poise-languages","~> 2.0"
